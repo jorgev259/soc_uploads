ad2fc1ba7b9c8e9d91333cc5a466bc408b66ca89
Jorge Vargas <jorgev259@outlook.es>
2019-01-20 17:06:55 -0600

    allow CORS

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const { get } = require('axios')
 const config = require('./config.json')
 
 const app = express()
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(busboy({
   highWaterMark: 2 * 1024 * 1024
 }))
