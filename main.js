691b48747582233b5bfa13d02db3c1c5c057a000
Jorge Vargas <jorgev259@outlook.es>
2019-01-22 10:35:54 -0600

    CORS

--- main.js
+++ main.js
@@ -9,20 +9,10 @@ var resumable = require('./resumable-node.js')('./tmp/')
 var multipart = require('connect-multiparty')
 var crypto = require('crypto')
 
+var cors = require('cors')
 const app = express()
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  next()
-})
+app.options('*', cors())
 app.use(multipart())
 
 const uploadPath = config.path
