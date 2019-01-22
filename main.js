3e2c0e15e7f6debb779ae04ee4cf8291ee3e0a46
Jorge Vargas <jorgev259@outlook.es>
2019-01-22 10:24:38 -0600

    CORS issue

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ const app = express()
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
   next()
 })
 app.use(multipart())
